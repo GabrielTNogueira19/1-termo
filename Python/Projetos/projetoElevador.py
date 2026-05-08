4b46fcc3dddeeab6810d906e850749c0296702e2
Gabriel Nogueira <gabriel.tnogueira19@gmail.com>
2026-05-08 11:20:32 -0300

    Atualização de Projeto de Elevador

---
+++ Python/Projetos/projetoElevador.py
@@ -0,0 +1,18 @@
+# Sistema de Elevador de Prédio
+# O prédio possui 10 andares, sendo o térro o andar 0. O elevador pode se mover para cima ou para baixo, e tem capacidade para 5 pessoas.
+# O elevador começa no andar 0 e pode ser chamado por qualquer pessoa em qualquer andar.
+# O elevador deve se mover para o andar onde a pessoa chamou, e depois para o andar de destino da pessoa.
+# O elevador deve exibir mensagens indicando o andar atual, o número de pessoas no elevador e as ações realizadas (subindo, descendo, parando).
+# O programa deve continuar rodando até que o usuário decida encerrar.
+# 
+# requisitos funcionais:
+# 1. O programa deve permitir que o elevador contenha no máximo 5 pessoas.
+# 2. O programa deve permitir que o usuário chame o elevador para um andar específico.
+# 3. O programa deve permitir que o usuário escolha um andar de destino após entrar no elevador.
+# 4. O programa deve exibir mensagens indicando o andar atual, o número de pessoas no elevador e as ações realizadas (subindo, descendo, parando).
+# 5. O programa deve continuar rodando até que o usuário decida encerrar.
+# 6. O elevado deve começar no andar 0.
+
+# requisitos não funcionais:
+# 1. O sistema deve impedir que o usuário digite um andar menor que 0 ou maior que 10.
+
